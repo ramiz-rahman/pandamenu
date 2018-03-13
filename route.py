95a3e3738e0af7125a5173e2b31ffeb71a04a914
Ramiz Rahman <ramiz.rahman@outlook.com>
2018-03-13 20:36:13 +0600

    Refactor: Make CRUD route names meaningful
    Rename URLs for each CRUD operation such that they become semantic.

--- route.py
+++ route.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 ############################# RESTAURANTS ######################################
 
 # Create
-@app.route('/restaurants/new/')
+@app.route('/restaurant/new/')
 def add_restaurant():
     """Create a new restaurant"""
 
@@ -21,13 +21,13 @@ def restaurants():
 
 
 # Update
-@app.route('/restaurants/<int:restaurant_id>/edit/')
+@app.route('/restaurant/<int:restaurant_id>/edit/')
 def edit_restaurant(restaurant_id):
     """Edit information of a particular restaurant"""
 
 
 # Delete
-@app.route('/restaurants/<int:restaurant_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/delete/')
 def delete_restaurant(restaurant_id):
     """Remove a particular restaurant"""
 
@@ -35,26 +35,26 @@ def delete_restaurant(restaurant_id):
 ############################# MENU ITEMS #######################################
 
 # Create
-@app.route('/restaurants/<int:restaurant_id>/menu_items/new/')
+@app.route('/restaurant/<int:restaurant_id>/menu/new/')
 def add_menu_item():
     """Create a new menu_item for particular restaurant"""
 
 
 # Read
-@app.route('/restaurants/<int:restaurant_id>/')
-@app.route('/restaurants/<int:restaurant_id>/menu_items/')
+@app.route('/restuarant/<int:restaurant_id>')
+@app.route('/restaurant/<int:restaurant_id>/menu/')
 def restaurant_menu(restaurant_id):
     """Display all menu items of particular restaurant"""
 
 
 # Update
-@app.route('/restaurants/<int:restaurant_id>/<int:item_id>/edit/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:item_id>/edit/')
 def edit_menu_item(restaurant_id, item_id):
     """Edit information of a particular item on the menu"""
 
 
 # Delete
-@app.route('/restaurants/<int:restaurant_id>/<int:item_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:item_id>/delete/')
 def delete_menu_item(restaurant_id, item_id):
     """Remove a particular item from the menu"""
 
